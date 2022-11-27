e4b6e2ae2e1202a2ab64756110f00333b39f6aee
kennedy maina <kenneyrit@gmail.com>
2022-11-27 20:13:43 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+const saturdayFun = function (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  };
+  saturdayFun();
+  saturdayFun("go shopping");
+  
+  const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  mondayWork();
+  mondayWork("go to the gym");
+  
+  const wrapAdjective = function (wrapper = "*") {
+    const message = function (string = "special") {
+      return `You are ${wrapper}${string}${wrapper}!`;
+    };
+  
+    return message;
+  };
+  
+  const result = wrapAdjective("||");
+  
+  const emphatic = result("a emphatic boss");
+  
+  console.log(emphatic);
